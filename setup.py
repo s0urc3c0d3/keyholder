5d919e04dd9351fe682a22bb156ab36055703c7c
Grzegorz DwornickiGrzegorz <gdwornicki@cloudferro.com>
2017-12-04 16:55:18 +0100

    Changed to 0.2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='keyholder',
+      version='0.2',
+      description='Wrapper for multiple key-value stores',
+      url='https://github.com/s0urc3c0d3/keyholder',
+      author='Grzegorz Dwornicki',
+      author_email='gd1100@gmail.com',
+      license='MIT',
+      packages=['keyholder'],
+      install_requires=['kazoo','python-etcd'],
+      zip_safe=False)
